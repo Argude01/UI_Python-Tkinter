a5cc9a0404bb11d344015b0c3cb891127cc70e15
ARGUDE10 <deyaniragutierrezarteaga@gmail.com>
2021-04-08 12:50:26 -0600

    Differents frames added

--- Hugo1.py
+++ Hugo1.py
@@ -2,19 +2,29 @@ from tkinter import *
 from tkinter import ttk
 
 window = Tk()
-f_home = Frame(window, width=400, height=800, bg="#d5d8de")
-f_home.pack()
+f_app = Frame(window, width=400, height=800, bg="red")
+f_app.pack()
 
-title = Label(f_home, text="Casa")
-title.grid(row=0, column=3)
-title = Label(f_home, text="Hola, Nicolas")
-title.grid(row=2, column=0)
-title = Label(f_home, text="¿Qué Necesitas?")
-title.grid(row=3, column=0)
+# Widgets dentro del contender APP
+f_navbar = Frame(f_app, width=400, height=100, bg="yellow")
+f_navbar.grid(row=0, column=0)
+f_title = Frame(f_app, width=400, height=100, bg="yellow")
+f_title.grid(row=1, column=0)
+f_options = Frame(f_app, width=400, height=400, bg="yellow")
+f_options.grid(row=2, column=0)
 
-f_item1 = Frame(f_home, width=300, height=300, bg="#6a93e6")
-f_item1.grid(row=5, column=1)
-f_item2 = Frame(f_home, width=300, height=300, bg="#d48df0")
-f_item2.grid(row=6, column=1)
+# Widgets dentro del contender OPTIONS
+f_food = Frame(f_options, width=400, height=200, bg="#d48df0")
+f_food.grid(row=0, column=0)
+f_drinks = Frame(f_options, width=400, height=200, bg="#d48df0")
+f_drinks.grid(row=1, column=0)
+
+# title = Label(f_app, text="Casa")
+# title.grid(row=0, column=3)
+# title = Label(f_app, text="Hola, Nicolas")
+# title.grid(row=2, column=0)
+# title = Label(f_app, text="¿Qué Necesitas?")
+# title.grid(row=3, column=0)
+
 
 window.mainloop()
